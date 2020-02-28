b135addf06c5214e2fe85c6cb931989326516363
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-27 21:23:53 -0800

    Makes the desk image responsive.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-
+      <div className='desk-y'></div>
+      <div className='desk-x'></div>
+      <div className='desk'></div>
     </div>
   );
 }

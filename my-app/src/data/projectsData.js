79b6de060ed9a659274589c21fd807db6c314719
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-29 13:50:13 -0800

    Creates a projects view data file with the urls of the LAS Toyota device screenshots.

---
+++ my-app/src/data/projectsData.js
@@ -0,0 +1,13 @@
+import iPhoneSE_V from '../images/LASToyota/iPhoneSE_V.png';
+import iPhoneSE_H from '../images/LASToyota/iPhoneSE_H.png';
+import iPhone8Plus_V from '../images/LASToyota/iPhone8Plus_V.png';
+import iPhone8Plus_H from '../images/LASToyota/iPhone8Plus_H.png';
+import iPad_V from '../images/LASToyota/iPad_V.png';
+import iPad_H from '../images/LASToyota/iPad_H.png';
+
+const LASTAppleDevices = [iPhoneSE_V, iPhoneSE_H, iPhone8Plus_V, iPhone8Plus_H, iPad_V, iPad_H];
+
+
+export { LASTAppleDevices };
+
+

98773b0f133d88a267eaa52ac8129d706052a15e
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-29 11:21:24 -0800

    Makes the css variable 'scale' in the Paper styled component become a prop instead.

--- my-app/src/views/EducationView.js
+++ my-app/src/views/EducationView.js
@@ -3,24 +3,23 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Paper = styled.div`
-	--scale: 0.7;
-	width: calc(var(--scale) * 8.5in);
-	height: calc(var(--scale) * 11in);
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	margin: auto;
-	background-color: white;
-	`;
+width: calc(${({ scale }) => scale * 8.5}in);
+height: calc(${({ scale }) => scale * 11}in);
+position: absolute;
+top: 0;
+bottom: 0;
+left: 0;
+right: 0;
+margin: auto;
+background-color: white;
+`;
 
 
 const EducationView = () => {
 	return (
 		<>
 			<Link to='/'>GO BACK</Link>
-			<Paper>
+			<Paper scale={0.7}>
 				<h2>Education</h2>
 			</Paper>
 		</>

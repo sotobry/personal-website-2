90a0ed594c705c817a3a442240c9158d6b73d1a1
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-29 15:15:25 -0800

    Organizes the Apple device components for the LAS Toyota project.

--- my-app/src/views/ProjectsView.js
+++ my-app/src/views/ProjectsView.js
@@ -1,20 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { LASTAppleDevicesData } from '../data/projectsData';
+import styled from 'styled-components';
+
+const DeviceGroups = styled.div`
+	width: 100vw;
+	height: 100vh;
+
+	position: absolute;
+	top: 0;
+
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+
+	padding: 100px;
+	z-index: -1;
+`;
+
+const DeviceGroup = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	&:nth-child(odd) {
+		flex-direction: column-reverse;
+	}
+
+	img {
+		margin: 10px -15px;
+	}
+`;
+
+const ImgV = styled.img`
+	width: ${({ mainDimension, scale }) => scale * mainDimension}in;
+`;
+
+const ImgH = styled.img`
+	height: ${({ mainDimension, scale }) => scale * mainDimension}in;
+`;
 
 const ProjectsView = (props) => {
 	const { scale } = props;
 	const LASTAppleDeviceGroupComponents = LASTAppleDevicesData.map(group =>
-		<div className='deviceGroup'>
-			<img src={group.imgURL_V} alt='' style={{ width: `${group.mainDimension * scale}in` }} />
-			<img src={group.imgURL_H} alt='' style={{ height: `${group.mainDimension * scale}in` }} />
-		</div>);
+		<DeviceGroup>
+			<ImgV scale={scale} src={group.imgURL_V} mainDimension={group.mainDimension} alt='' />
+			<ImgH scale={scale} src={group.imgURL_H} mainDimension={group.mainDimension} alt='' />
+		</DeviceGroup>);
 
 	return (
 		<>
 			<Link to='/'>GO BACK</Link>
-			<h1>Projects</h1>
-			{LASTAppleDeviceGroupComponents}
+			<DeviceGroups>
+				{LASTAppleDeviceGroupComponents}
+			</DeviceGroups>
 		</>
 	);
 }

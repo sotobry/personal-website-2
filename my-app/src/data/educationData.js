a488d18ee9d89fbddff1bc82a017a48131ad8332
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-02-29 11:47:03 -0800

    Adds content to the paper component in the education section.

---
+++ my-app/src/data/educationData.js
@@ -0,0 +1,109 @@
+
+const EducationData = [
+	{
+		name: 'React',
+		courses: [
+			{
+				title: 'Scrimba\'s Advanced React Bootcamp',
+				instructor: 'Bob Ziroll'
+			},
+			{
+				title: 'Scrimba\'s Learn React: A Comprehensive Introduction',
+				instructor: 'Bob Ziroll'
+			}
+		]
+	},
+	{
+		name: 'JavaScript',
+		courses: [
+			{
+				title: 'Just JavaScript',
+				instructor: 'Dan Abramov'
+			},
+			{
+				title: 'Eloquent JavaScript',
+				instructor: 'Marijn Haverbeke'
+			}
+		]
+	},
+	{
+		name: 'CSS',
+		courses: [
+			{
+				title: 'Grid Critters, Mastering CSS Grids',
+				instructor: 'Dave Geddes'
+			},
+			{
+				title: 'Flexbox Zombies, Mastering CSS Flexbox',
+				instructor: 'Dave Geddes'
+			},
+			{
+				title: 'CSS Essential Training 3',
+				instructor: 'Christina Truong'
+			},
+			{
+				title: 'CSS Essential Training 2',
+				instructor: 'Christina Truong'
+			},
+			{
+				title: 'CSS Essential Training 1',
+				instructor: 'Christina Truong'
+			}
+		]
+	},
+	{
+		name: 'HTML',
+		courses: [
+			{
+				title: 'HTML: Structured Data',
+				instructor: 'James Williamson'
+			},
+			{
+				title: 'HTML Essential Training',
+				instructor: 'James Williamson'
+			}
+		]
+	},
+	{
+		name: 'Git',
+		courses: [
+			{
+				title: 'Git Branches, Merges, and Remotes',
+				instructor: 'Kevin Skoglund'
+			},
+			{
+				title: 'Git Essential Training: The Basics',
+				instructor: 'Kevin Skoglund'
+			}
+		]
+	},
+	{
+		name: 'VS Code',
+		courses: [
+			{
+				title: 'Visual Studio Code for Web Developers',
+				instructor: 'Joe Marini'
+			}
+		]
+	},
+	{
+		name: 'Unix',
+		courses: [
+			{
+				title: 'Unix for Mac OS X Users',
+				instructor: 'Kevin Skoglund'
+			}
+		]
+	},
+	{
+		name: 'Computer Science',
+		courses: [
+			{
+				title: 'Harvard\'s CS50',
+				instructor: 'David J. Maland'
+			}
+		]
+	}
+];
+
+export { EducationData };
--- my-app/src/views/EducationView.js
+++ my-app/src/views/EducationView.js
@@ -1,26 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { EducationData } from '../data/educationData';
 
 const Paper = styled.div`
 width: calc(${({ scale }) => scale * 8.5}in);
 height: calc(${({ scale }) => scale * 11}in);
+
 position: absolute;
 top: 0;
 bottom: 0;
 left: 0;
 right: 0;
+
 margin: auto;
+padding: calc(${({ scale }) => scale * 0.5}in);
+
 background-color: white;
+
+	ul, li {
+		list-style-type: none;
+	}
+
+	& > ul {
+		height: calc(${({ scale }) => scale * 10}in);
+		width: calc(${({ scale }) => scale * 3.75}in);
+		
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+	}
+
+	h1, .courses, .courses li {
+		margin-bottom: 20px;
+	}
+
+	.courses li {
+		margin-right: calc(${({ scale }) => scale * 0.5}in);
+		margin-left: calc(${({ scale }) => scale * 0.25}in);
+	}
+
+	h2, h3 {
+		font-weight: normal;
+	}
+
+	h1 { font-size: 20px; }
+	h2 { font-size: 14px; }
+	h3 { font-size: 12px; }
 `;
 
 
 const EducationView = () => {
+	const education = EducationData.map(subject =>
+		<li>
+			<h1>{subject.name}</h1>
+			<ul className='courses'>
+				{subject.courses.map(course =>
+					<li>
+						<h2>{course.title} by <i>{course.instructor}</i></h2>
+					</li>
+				)}
+			</ul>
+		</li>
+	);
 	return (
 		<>
 			<Link to='/'>GO BACK</Link>
 			<Paper scale={0.7}>
-				<h2>Education</h2>
+				<ul>
+					{education}
+				</ul>
 			</Paper>
 		</>
 	);

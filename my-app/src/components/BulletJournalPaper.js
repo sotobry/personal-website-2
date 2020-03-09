276f8c8c5612a6017990de29a3a4ec6253a1ef40
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-03-08 17:57:05 -0700

    Styles a page to be like the bullet journal page with a grid of dots.

--- my-app/src/components/BulletJournalPaper.js
+++ my-app/src/components/BulletJournalPaper.js
@@ -2,18 +2,53 @@ import React from 'react';
 import styled from 'styled-components';
 
 const PaperSC = styled.div`
-	width: 300px;
-	height: 300px;
-	background-color: red;
+	width: ${({ scale }) => scale * 5.75}in;
+	height: ${ ({ scale }) => scale * 8.25}in;
+	background-color: #f7f8f3;
 	position: absolute;
 	top: 0;
 	bottom: 0;
 	right: 0;
 	left: 0;
 	margin: auto auto;
+	display: grid;
+
+	grid-template-columns: repeat(27, 1fr);
+	grid-template-rows: repeat(39, 1fr);
+	padding: 20px 15px;
 `;
 
+const DotSC = styled.div`
+	width: 1px;
+	height: 1px;
+	border-radius: 50%;
+	background-color: #746060;
+	grid-row: 0 -1;
+	display: inline-block;
+	grid-column: 0 -1;
+`;
+
+const Row = () => <>
+	<DotSC /><DotSC /><DotSC /><DotSC /><DotSC />
+	<DotSC /><DotSC /><DotSC /><DotSC /><DotSC />
+	<DotSC /><DotSC /><DotSC /><DotSC /><DotSC />
+	<DotSC /><DotSC /><DotSC /><DotSC /><DotSC />
+	<DotSC /><DotSC /><DotSC /><DotSC /><DotSC />
+	<DotSC /><DotSC />
+</>
 const BulletJournalPaper = () =>
-	<PaperSC>This is a paper</PaperSC>;
+	<PaperSC scale={0.8}>
+		<Row /><Row /><Row /><Row /><Row />
+		<Row /><Row /><Row /><Row /><Row />
+		<Row /><Row /><Row />
+
+		<Row /><Row /><Row /><Row /><Row />
+		<Row /><Row /><Row /><Row /><Row />
+		<Row /><Row /><Row />
+
+		<Row /><Row /><Row /><Row /><Row />
+		<Row /><Row /><Row /><Row /><Row />
+		<Row /><Row /><Row />
+	</PaperSC >;
 
 export { BulletJournalPaper }

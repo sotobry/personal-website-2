4ebc4224ec0114d5699a0fcd32596c1db4330158
Bryann Sotomayor-Rinaldi <bryann.sotomayor@gmail.com>
2020-03-09 13:40:34 -0700

    Adds a 2nd page.

--- my-app/src/components/BulletJournalPaper.js
+++ my-app/src/components/BulletJournalPaper.js
@@ -5,17 +5,13 @@ const PaperSC = styled.div`
 	width: ${({ scale }) => scale * 5.75}in;
 	height: ${ ({ scale }) => scale * 8.25}in;
 	background-color: #f7f8f3;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	right: 0;
-	left: 0;
-	margin: auto auto;
+	position: relative;
 	display: grid;
-
+	/* width: 100%; */
 	grid-template-columns: repeat(27, 1fr);
 	grid-template-rows: repeat(39, 1fr);
-	padding: 20px 15px;
+		padding: 20px 15px;
+
 	`;
 
 const DotSC = styled.div`
@@ -24,29 +20,33 @@ const DotSC = styled.div`
 	border-radius: 50%;
 	background-color: #746060;
 	grid-row: 0 -1;
-	display: inline-block;
 	grid-column: 0 -1;
+	display: inline-block;
+
 	`;
 
 const Text = styled.div`
 	position: absolute;
+
 	font-size: 16px;
 	font-family: 'Meie Script';
 	padding: 20px 30px;
-	/* width: 50%; */
 	height: 100%;
+	width: inherit;
+	height: inherit;
+
+	column-count: 2;
 	column-fill: auto;
 	column-gap: 50px;
 	column-rule-style: solid;
 	column-rule-width: 1px;
 	column-rule-color: black;
-	/* height: 95%; */
+
 	color: #0000d8;
 	hyphens: auto;
 	text-shadow: #0000dd 0px 0.5px;
 	line-height: 1;
-	/* border-right: black solid 1px;; */
-	column-count: 2;
+
 `;
 
 const Row = () => <>
--- my-app/src/views/BlogView.js
+++ my-app/src/views/BlogView.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { GoBackButton } from '../components/GoBackButton';
 import { BulletJournalPaper } from '../components/BulletJournalPaper';
+import styled from 'styled-components';
 
+const BulletJournal = styled.div`
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	right: 0;
+	left: 0;
+	margin: auto auto;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
 
 const BlogView = () => {
 
@@ -9,21 +22,14 @@ const BlogView = () => {
 		<>
 			<GoBackButton />
 			<h1>This is the blog section.</h1>
-			<div className='journal'>
-				<div className='leftPage'><h2>Left Page</h2></div>
-				<div className='rightPage'><h2>Right Page</h2></div>
-
-
-
-
-
-
-
-
+			<BulletJournal>
 				<BulletJournalPaper>
 					I've been feeling quite stressed lately. I've applied to so many jobs, so many positions, and yet no one has been willing to give me a technichal interview with the exception of Nintendo for a position I was underqualified. I'm almost sure it is because I do not have a bachelor's degree, and because I do not have experience. But I need a bachelor's degree to get experience. I've applied to over 500 positions. Yes, hundreds. And so far just 1 technical interview. That's just crazy to me. I've ran out of my savings. I cannot longer afford to keep living this lifestyle. But I really don't want to go back to customer service related jobs. I hate them so much. They always end up paying me minimum wage even though I have over 5 years of experience in that field. And they look down on me. I hate customer service jobs. I want to do something else. I want to think. I want to create, fix, improve something. I can do so much more than carrying a plate of food to a table, handling money, cleaning toilets. I can do so much more than that. Why does society insists in not giving me a chance?
-					</BulletJournalPaper>
-			</div>
+				</BulletJournalPaper>
+				<BulletJournalPaper>
+					Page 2.
+				</BulletJournalPaper>
+			</BulletJournal>
 		</>
 	);
 }
